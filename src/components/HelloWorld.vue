0b5ebe20b9691094590f9a5795e87c01609a1a34
alexbabich <ababych.m@gmail.com>
2018-01-03 18:51:17 +0200

    setup project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-container class="hello mt-4 mb-4">
+    <h1>{{ msg }}</h1>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'todo list'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  * {
+    font-size: 14px;
+  }
+</style>
